629115c5ac83d4ca3c367159431c3ed903794d8e
elijah-hopper848 <ehopper848@phm.k12.in.us>
2025-10-31 11:19:31 -0400

    correct custom scoring

--- main.py
+++ main.py
@@ -1,115 +1,113 @@
 from random import choices
 
 def choose_random_weighted(list: dict[str, int]):
-  options = []
-  weights = []
+	options = []
+	weights = []
 
-  for key, weight in list.items():
-    options.append(key)
-    weights.append(weight)
+	for key, weight in list.items():
+		options.append(key)
+		weights.append(weight)
 
-  return choices(options, weights=weights, k=1)[0]
-
+	return choices(options, weights=weights, k=1)[0]
 def choose_best(list: dict[str, int]) -> str:
-  best = ""
-  record = 0
-  
-  for key, count in list.items():
-    if count > record:
-      best = key
-      record = count
-  
-  return best
+	best = ""
+	record = 0
+	
+	for key, count in list.items():
+		if count > record:
+			best = key
+			record = count
+	
+	return best
 
 def score_pattern(reference: str, pattern: str) -> int:
-  score = 0
+	score = 0
 
-  if pattern.lower() == reference.lower():
-    score += 400 # arbitrarily good score, because it directly matches
+	max_length = min(len(reference), len(pattern))
+	min_pattern = pattern[-max_length:]
+	min_reference = reference[-max_length:]
+	
+	if min_pattern == min_reference:
+		score += 400
 
-  for index in range(min(len(reference), len(pattern))):
-    ref_char = reference[index]
-    pat_char = pattern[index]
+	for index in range(max_length):
+		ref_char = min_reference[index]
+		pattern_char = min_pattern[index]
 
-    if ref_char == pat_char:
-      score += 70
-    elif ref_char.lower() == pat_char.lower():
-      score += 40
+		if ref_char == pattern_char:
+			score += 80
+		elif ref_char.lower() == pattern_char.lower():
+			score += 40
 
-  return score
+	return score
+def determine_closest_pattern(pattern_list: dict[str, dict[str, int]], pattern: str) -> str:
+	best = pattern
+	record = -1
 
-def determine_best_pattern(references: dict[str, dict[str, int]], pattern: str) -> str:
-  best = ""
-  record = 0
+	for reference in pattern_list:
+		score = score_pattern(reference, pattern)
 
-  for reference in references:
-    score = score_pattern(reference, pattern)
+		# print(f"score: {score:-3} | ref: {reference!r} | pat: {pattern!r}")
 
-    if score > record:
-      record = score
-      best = reference
-  
-  return best
-
-def extract_pattern(text: str, index: int, width: int) -> str:
-  return text[index - width:index]
+		if score > record:
+			record = score
+			best = reference
+	
+	return best
 
 def analyze_text(text: str, pattern_width: int, association_width: int) -> dict[str, dict[str, int]]:
-  output_patterns = {}
-  
-  for index in range(pattern_width, len(text) - association_width + 1):
-    pattern = extract_pattern(text, index, pattern_width)
-    association = text[index:index+association_width]
+	output_patterns = {}
+	
+	for index in range(pattern_width, len(text) - association_width + 1):
+		pattern = text[index-pattern_width:index]
+		associated_text = text[index:index+association_width]
 
-    if pattern not in output_patterns:
-      output_patterns[pattern] = {}
+		if pattern not in output_patterns:
+			output_patterns[pattern] = {}
 
-    pattern_dict = output_patterns[pattern]
-    
-    if association not in pattern_dict:
-      pattern_dict[association] = 1
-    else:
-      pattern_dict[association] += 1
+		pattern_dict = output_patterns[pattern]
+		
+		if associated_text not in pattern_dict:
+			pattern_dict[associated_text] = 1
+		else:
+			pattern_dict[associated_text] += 1
 
-  return output_patterns
-
+	return output_patterns
 def generate_pseudo_english(patterns: dict[str, dict[str, int]], seed: str, length: int, pattern_width: int) -> str:
-  output = seed
+	output = seed
 
-  i = 0
+	while len(output) < length:
+		cursor = len(output)
 
-  while i < length:
-    current_pattern = extract_pattern(output, i + pattern_width, pattern_width)
-    best_pattern = determine_best_pattern(patterns, current_pattern)
+		current_pattern = output[cursor-pattern_width:cursor]
+		closest_pattern = determine_closest_pattern(patterns, current_pattern)
 
-    print(current_pattern, best_pattern)
+		# print(f"{cursor:3}:|{current_pattern!r}|{closest_pattern!r}|")
 
-    associations = patterns[best_pattern]
+		associations = patterns[closest_pattern]
 
-    next_token = choose_random_weighted(associations)
+		next_token = choose_random_weighted(associations)
 
-    output += next_token
-    i += len(next_token)
-  
-  return output.strip()
+		output += next_token
+	
+	return output.strip()
 
 INPUT_DATA = ""
-PATTERN_SIZE = 5
-
+PATTERN_SIZE = 6
 
 # with open("grant_data.txt", "r") as f:
 #   test_data += f.read()
 
 with open("whitman_leaves_of_grass.txt", "+r") as f:
-  INPUT_DATA += f.read()
+	INPUT_DATA += f.read()
 
-pattern_associations = analyze_text(INPUT_DATA, PATTERN_SIZE, 4)
+pattern_associations = analyze_text(INPUT_DATA, PATTERN_SIZE, 6)
 
-print(f"input data length: {len(INPUT_DATA)}\n")
+print(f"input data length: {len(INPUT_DATA)}\npattern size: {PATTERN_SIZE}")
 # print("pattern associations:\n", pattern_associations)
 
 while True:
-  seed = input("Seed: ")
-  output_text = generate_pseudo_english(pattern_associations, seed, 500, PATTERN_SIZE)
+	seed = input("seed: ")
 
-  print(f"{'-' * 60}\n{output_text}\n{'-' * 60}")
+	output_text = generate_pseudo_english(pattern_associations, seed, 500, 4)
+	print(f"{"-" * 50}\n{output_text}\n{'-' * 50}")
